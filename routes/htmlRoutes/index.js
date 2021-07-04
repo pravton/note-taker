19212080b6514e02837f62b7fbf43ec060259256
PravTon <clintonsf@outlook.com>
2021-07-04 15:16:59 -0400

    updated comments

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const router = require('express').Router();
 
+// Get route for notes.html
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
+// Get route for index.html
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
+// Get route for all other end points
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
